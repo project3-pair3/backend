20030483237d6a25e16e5d14f3bc3cd24735e259
Ga Min Cha <prettygamin@naver.com>
2026-05-13 11:06:37 +0900

    S3 api endpoint 수정

--- src/main/java/com/example/project3/s3/controller/S3Controller.java
+++ src/main/java/com/example/project3/s3/controller/S3Controller.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/s3")
+@RequestMapping("/s3")
 @RequiredArgsConstructor
 public class S3Controller {
 
