fac71ec8e63e320f28665cb0226d5f45f943630e
Ga Min Cha <prettygamin@naver.com>
2026-05-13 16:27:10 +0900

    data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -55,10 +55,10 @@ INSERT INTO cafe (cafe_name, user_id, address_city, address_district, address_de
 VALUES ('서초구1 카페', 4, '서울시', '서초구', '테헤란로 123', '도심 속 조용한 휴식 공간입니다.', '09:00:00', '22:00:00', 'https://images.unsplash.com/photo-1509042239860-f550ce710b93', '2026-05-11 05:21:52', NOW());
 
 INSERT INTO cafe (cafe_name, user_id, address_city, address_district, address_detail, description, opening_time, closing_time, image_url, created_at, updated_at)
-VALUES ('서초구2 리프 카페', 5, '서울시', '강남구', '강남대로 545', '자연 친화적인 인테리어와 신선한 원두를 제공합니다.', '09:00:00', '22:00:00', 'https://images.unsplash.com/photo-1495474472287-4d71bcdd2085', '2026-05-11 05:21:52', NOW());
+VALUES ('서초구2 리프 카페', 5, '서울시', '서초구', '강남대로 545', '자연 친화적인 인테리어와 신선한 원두를 제공합니다.', '09:00:00', '22:00:00', 'https://images.unsplash.com/photo-1495474472287-4d71bcdd2085', '2026-05-11 05:21:52', NOW());
 
 INSERT INTO cafe (cafe_name, user_id, address_city, address_district, address_detail, description, opening_time, closing_time, image_url, created_at, updated_at)
-VALUES ('서초구3 뷰 커피', 6, '서울시', '강남구', '강남대로102길 21 지하1, 지상 1-3층', '탁 트인 바다를 보며 커피 한 잔의 여유를 즐기세요.', '09:00:00', '22:00:00', 'https://images.unsplash.com/photo-1501339819358-683c33f59630', '2026-05-11 05:21:52', NOW());
+VALUES ('서초구3 뷰 커피', 6, '서울시', '서초구', '강남대로102길 21 지하1, 지상 1-3층', '탁 트인 바다를 보며 커피 한 잔의 여유를 즐기세요.', '09:00:00', '22:00:00', 'https://images.unsplash.com/photo-1501339819358-683c33f59630', '2026-05-11 05:21:52', NOW());
 
 -- 메뉴 데이터 삽입
 -- 1번 카페 (스타리 카페) 메뉴
